f66f971888744ab454c8876c7f579c67d1eb06a1
Edward Terry <edward.william.terry@gmail.com>
2020-05-16 08:28:22 -0700

    Get uv coordinates of intersected object

--- final_project/js/env.js
+++ final_project/js/env.js
@@ -1,9 +1,36 @@
-AFRAME.registerComponent('collider-check', {
-    dependencies: ['raycaster'],
-  
-    init: function () {
-      this.el.addEventListener('raycaster-intersection', function () {
-        console.log('Player hit something!');
-      });
+AFRAME.registerComponent("collider-check", {
+  dependencies: ["raycaster"],
+
+  init: function () {
+    this.el.addEventListener("raycaster-intersection", function () {
+      console.log("Player hit something!");
+    });
+  },
+});
+
+AFRAME.registerComponent("raycaster-listen", {
+  init: function () {
+    // Use events to figure out what raycaster is listening so we don't have to
+    // hardcode the raycaster.
+    this.el.addEventListener("raycaster-intersected", (evt) => {
+      this.raycaster = evt.detail.el;
+    });
+    this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
+      this.raycaster = null;
+    });
+  },
+
+  tick: function () {
+    if (!this.raycaster) {
+      return;
+    } // Not intersecting.
+
+    let intersection = this.raycaster.components.raycaster.getIntersection(
+      this.el
+    );
+    if (!intersection) {
+      return;
     }
-  });
+    console.log(intersection.uv);
+  },
+});

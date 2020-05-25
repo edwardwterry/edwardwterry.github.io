c158483d95e8c4597404104f4a22764eed39244d
Edward Terry <edward.william.terry@gmail.com>
2020-05-24 21:25:50 -0700

    Publish OSC message with unique intersected object ID and uv

--- final_project/public/js/sketch.js
+++ final_project/public/js/sketch.js
@@ -5,20 +5,27 @@ let target_canvas;
 let u, v;
 let myp5;
 
+var oscPort = new osc.WebSocketPort({
+  url: "ws://localhost:3000", // URL to your Web Socket server. // TODO change from localhost?
+  metadata: true,
+});
+
+oscPort.open();
+
 $(document).ready(function () {
-  socket = io.connect();
+  // socket = io.connect();
 
   // https://gist.github.com/wanbinkimoon/0771fea9b199ce5ac32edc8f6d815584
   const sketch = (p) => {
-    let width = 150;
-    let height = 75;
+    let width = 300;
+    let height = 200;
+    let count = 0;
     p.setup = () => {
       c = p.createCanvas(width, height);
       c.background(255, 255, 255);
       c.strokeWeight(20);
       p.fill(0);
     };
-
     p.draw = () => {
       // https://stackoverflow.com/questions/50966769/drawing-p5-js-canvas-inside-a-html-canvas-using-drawimage
 
@@ -26,11 +33,14 @@ $(document).ready(function () {
       var HTMLcontext = HTMLcanvas.getContext("2d");
       // if (spacebar) {
       // console.log("frame count", p.frameCount);
+      c.background(30, 30, 30);
       p.point(p.frameCount, 30);
-      // console.log(p.int(u * width), p.int(v * height));
+      console.log(p.int(u * width), p.int(v * height));
+      // console.log(u, v);
       // }
       // https://stackoverflow.com/questions/50966769/drawing-p5-js-canvas-inside-a-html-canvas-using-drawimage
       HTMLcontext.drawImage(c.canvas, 0, 0);
+      count++;
     };
   };
 
@@ -51,34 +61,53 @@ AFRAME.registerComponent("collider-check", {
 // RAYCASTING
 
 // helps to get the properties of the intersected elements
-AFRAME.registerComponent("raycaster-listen", {
-  init: function () {
-    // Use events to figure out what raycaster is listening so we don't have to
-    // hardcode the raycaster.
-    this.el.addEventListener("raycaster-intersected", (evt) => {
-      this.raycaster = evt.detail.el;
-    });
-    this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
-      this.raycaster = null;
-    });
-  },
+oscPort.on("ready", function () {
+  AFRAME.registerComponent("raycaster-listen", {
+    init: function () {
+      // Use events to figure out what raycaster is listening so we don't have to
+      // hardcode the raycaster.
+      this.el.addEventListener("raycaster-intersected", (evt) => {
+        this.raycaster = evt.detail.el;
+      });
+      this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
+        this.raycaster = null;
+      });
+    },
 
-  tick: function () {
-    if (!this.raycaster) {
-      return;
-    } // Not intersecting.
+    tick: function () {
+      if (!this.raycaster) {
+        return;
+      } // Not intersecting.
 
-    let intersection = this.raycaster.components.raycaster.getIntersection(
-      this.el
-    );
-    if (!intersection) {
-      return;
-    }
-    u = intersection.uv["x"];
-    // v = intersection.uv["y"];
-    // u = 1.0 - intersection.uv["x"];
-    v = 1.0 - intersection.uv["y"];
-  },
+      let intersection = this.raycaster.components.raycaster.getIntersection(
+        this.el
+      );
+      if (!intersection) {
+        return;
+      }
+      u = intersection.uv["x"];
+      // v = intersection.uv["y"];
+      // u = 1.0 - intersection.uv["x"];
+      v = 1.0 - intersection.uv["y"];
+      oscPort.send({
+        address: "/wall_ray",
+        args: [
+          {
+            type: "i",
+            value: intersection.object.id,
+          },
+          {
+            type: "f",
+            value: u,
+          },
+          {
+            type: "f",
+            value: v,
+          },
+        ],
+      });
+    },
+  });
 });
 
 // --------------------------
--- final_project/server.js
+++ final_project/server.js
@@ -1,21 +1,43 @@
 const express = require('express');
-const app     = express();
-const PORT    = process.env.PORT || 3000;
-const server  = require('http').createServer(app);
-const io      = require('socket.io')(server);
+const osc = require('osc');
+const WebSocket = require('ws');
+const WEB_SERVER_PORT = process.env.PORT || 3000;
 
-io.on('connection', function (socket) {
-  console.log("New socket client connection: ", socket.id);
+// Create an express web server
+const app = express();
+const server = app.listen(WEB_SERVER_PORT, () => {
+  console.log('Web server listening on port', WEB_SERVER_PORT);
 });
 
-// --------------------------------------------------------
-// EXPRESS STUFF
-// --------------------------------------------------------
-// tell our app where to serve our static files
+// expose the local public folder for inluding files js, css etc..
 app.use(express.static('public'));
 
-// --------------------------------------------------------
-// tell our app where to listen for connections
-server.listen(PORT, () => {
-  console.log('Listening on PORT ' + PORT);
-});
+// Create a WebSocket server
+const wss = new WebSocket.Server({ server: server });
+
+// Wait for a WebSocket connection
+wss.on('connection', (socket, request) => {
+  // Create an OSC port over the WebSocket
+  const webSocketPort = new osc.WebSocketPort({ socket: socket });
+
+  // When messages arrive, print them to the console and send them over the network
+  webSocketPort.on('message', (msg) => {
+    console.log('Web to network:', msg);
+  });
+
+  // Handle errors
+  webSocketPort.on('error', (error) => {
+    console.error(error);
+  });
+
+  webSocketPort.on('close', () => {
+    console.log(request.socket.remoteAddress, 'disconnected');
+  });
+
+  // Store the OSC port in the socket object so we can retrieve it later
+  socket.oscPort = webSocketPort;
+
+  // Open the OSC port
+  webSocketPort.open();
+  console.log(request.socket.remoteAddress, 'connected');
+})

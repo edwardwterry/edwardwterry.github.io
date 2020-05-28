4ee7bac96006895787ef63c0e6cac4ec822621fd
Edward Terry <edward.william.terry@gmail.com>
2020-05-27 18:30:10 -0700

    Sketch updates on a plane

--- final_project/public/js/sketch.js
+++ final_project/public/js/sketch.js
@@ -11,65 +11,96 @@ var oscPort = new osc.WebSocketPort({
 });
 
 oscPort.open();
-
+let test_incr = 0;
 // $(document).ready(function () {
-  // socket = io.connect();
-let tick_count = 0;
-setInterval(function(){
-  var ctx = document.getElementById('custom-canvas').getContext('2d');
-  ctx.beginPath();
-  ctx.rect(20, 20, 150, 100);
-  ctx.fillStyle = "red";
-  ctx.fill();
-  // ctx.fill(tick_count * 20, tick_count * 20, tick_count * 20);
-  tick_count++;
-  console.log("in set interval");
-}, 1000);
-  // https://gist.github.com/wanbinkimoon/0771fea9b199ce5ac32edc8f6d815584
-  const sketch = (p) => {
-    let width = 3000;
-    let height = 150;
-    let count = 0;
-    p.setup = () => {
-      c2 = p.createCanvas(width, height);
-      c = p.createCanvas(width, height);
-      // c.parent('custom-canvas');
-      c.background(40, 40, 40);
-      c2.background(200, 200, 200);
-      c.strokeWeight(5);
-      c2.strokeWeight(5);
-      p.fill(0);
-    };
-    p.draw = () => {
-      // https://stackoverflow.com/questions/50966769/drawing-p5-js-canvas-inside-a-html-canvas-using-drawimage
-
-      var HTMLcanvas = document.getElementById("custom-canvas");
-      var HTMLcontext = HTMLcanvas.getContext("2d");
-      // if (spacebar) {
-      // console.log("frame count", p.frameCount);
-      // c.background(count*50, count*50, count*50);
-      // c2.background(0,0,0);
-      c.point(count*5, 30);
-      c2.point(count*5, 30);
-      // console.log(p.int(u * width), p.int(v * height));
-      // console.log(count*10);
-      if (count<5){
-        console.log(HTMLcontext);
-
-      }
-      // }
-      // https://stackoverflow.com/questions/50966769/drawing-p5-js-canvas-inside-a-html-canvas-using-drawimage
-      HTMLcontext.drawImage(c.canvas, 0, 0);
-      if (count>100){
-        HTMLcontext.drawImage(c2.canvas, 0, 0);
-      }
-      // HTMLcontext.texture
-      count++;
-      console.log(count);
-    };
+// socket = io.connect();
+// let tick_count = 0;
+// setInterval(function(){
+//   var ctx = document.getElementById('custom-canvas').getContext('2d');
+//   ctx.beginPath();
+//   ctx.rect(20, 20, 150, 100);
+//   ctx.fillStyle = "red";
+//   ctx.fill();
+//   // ctx.fill(tick_count * 20, tick_count * 20, tick_count * 20);
+//   tick_count++;
+//   console.log("in set interval");
+// }, 1000);
+// https://gist.github.com/wanbinkimoon/0771fea9b199ce5ac32edc8f6d815584
+const sketch = (p) => {
+  let width = 3000;
+  let height = 150;
+  let count = 0;
+  p.setup = () => {
+    // c2 = p.createCanvas(width, height);
+    c = p.createCanvas(width, height);
+    // c.parent('custom-canvas');
+    c.background(200, 200, 200);
+    // c2.background(200, 200, 200);
+    c.strokeWeight(5);
+    // c2.strokeWeight(5);
+    p.fill(0);
   };
+  p.draw = () => {
+    // https://stackoverflow.com/questions/50966769/drawing-p5-js-canvas-inside-a-html-canvas-using-drawimage
 
-  // myp5 = new p5(sketch);
+    var HTMLcanvas = document.getElementById("custom-canvas");
+    var HTMLcontext = HTMLcanvas.getContext("2d");
+    // if (spacebar) {
+    // console.log("frame count", p.frameCount);
+    // c.background(count*50, count*50, count*50);
+    // c2.background(0,0,0);
+    c.point(count * 5, 30);
+    // c2.point(count * 5, 30);
+    // console.log(p.int(u * width), p.int(v * height));
+    // console.log(count*10);
+    // if (count < 5) {
+    //   console.log(HTMLcontext);
+    // }
+    // }
+    // https://stackoverflow.com/questions/50966769/drawing-p5-js-canvas-inside-a-html-canvas-using-drawimage
+    HTMLcontext.drawImage(c.canvas, 0, 0);
+    // if (count > 100) {
+    //   HTMLcontext.drawImage(c2.canvas, 0, 0);
+    // }
+    // HTMLcontext.texture
+    count++;
+    console.log(count);
+  };
+};
+
+myp5 = new p5(sketch)
+
+
+// AFRAME.registerComponent("draw-canvas", {
+//   schema: { default: "" },
+
+//   init: function () {
+//     this.canvas = document.getElementById(this.data);
+//     this.ctx = this.canvas.getContext("2d");
+//     console.log("Init!");
+//     // // Draw on canvas...
+//   },
+//   tick: () => {
+//     this.ctx.beginPath();
+//     this.ctx.rect(20, 20, 150, 100);
+//     this.ctx.fillStyle = "rgba(" + test_incr + ",100,100,100)";
+//     this.ctx.fill();
+//     test_incr++;
+//   }
+// });
+
+AFRAME.registerComponent('canvas-updater', {
+  dependencies: ['geometry', 'material'],
+
+  tick: function () {
+    var el = this.el;
+    var material;
+
+    material = el.getObject3D('mesh').material;
+    if (!material.map) { return; }
+    material.map.needsUpdate = true;
+  }
+});
 
 // });
 
@@ -84,10 +115,10 @@ AFRAME.registerComponent("collider-check", {
 });
 
 AFRAME.registerComponent("screen", {
-  update: function() {
+  update: function () {
     var material = new THREE.MeshBasicMaterial({
       // color: "green",
-      wireframe: false
+      wireframe: false,
     });
 
     var geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
@@ -96,7 +127,7 @@ AFRAME.registerComponent("screen", {
     material.map = canvasMap;
     material.map.needsUpdate = true;
     material.needsUpdate = true;
-    this.el.setObject3D('mesh', new THREE.Mesh(geometry, material));
+    this.el.setObject3D("mesh", new THREE.Mesh(geometry, material));
   },
 });
 

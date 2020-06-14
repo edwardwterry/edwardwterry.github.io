43fc96f14fe960931b837688fc499468f4c76d9d
Edward Terry <edward.william.terry@gmail.com>
2020-06-14 08:50:07 -0700

    Map ball height to pitch but only updates once

--- final_project/public/js/sketch.js
+++ final_project/public/js/sketch.js
@@ -15,6 +15,13 @@ let expiry = 180; //secs
 let ripple_u = 0.0;
 let ripple_v = 0.0;
 
+// tone setup
+let synth;
+let seq;
+let bpm = 70;
+
+let ball_scale_fract;
+let ball_scale;
 
 var oscPort = new osc.WebSocketPort({
   url: "ws://localhost:3000", // URL to your Web Socket server. // TODO change from localhost?
@@ -23,7 +30,6 @@ var oscPort = new osc.WebSocketPort({
 
 oscPort.open();
 $(document).ready(function () {
-
   const sketch = (p) => {
     let width = 512;
     let height = 512;
@@ -43,8 +49,8 @@ $(document).ready(function () {
       // The following line initializes a 2D cols-by-rows array with zeroes
       // in every array cell, and is equivalent to this Processing line:
       // current = new float[cols][rows];
-      current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
-      previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+      current = new Array(cols).fill(0).map((n) => new Array(rows).fill(0));
+      previous = new Array(cols).fill(0).map((n) => new Array(rows).fill(0));
     };
 
     p.draw = () => {
@@ -56,7 +62,7 @@ $(document).ready(function () {
       // console.log(x_pix, z_pix);
       previous[x_pix][z_pix] = 500;
       // previous[235][301] = 500;
-      p.background(87,185,255);
+      p.background(87, 185, 255);
 
       p.loadPixels();
       for (let i = 1; i < cols - 1; i++) {
@@ -79,7 +85,7 @@ $(document).ready(function () {
         }
       }
       p.updatePixels();
-    
+
       let temp = previous;
       previous = current;
       current = temp;
@@ -88,37 +94,42 @@ $(document).ready(function () {
     };
   };
 
-  myp5 = new p5(sketch);  
+  myp5 = new p5(sketch);
 
   var HTMLcanvas = document.getElementById("forest-canvas");
   var HTMLcontext = HTMLcanvas.getContext("2d");
-  let img = document.getElementById('forest');
+  let img = document.getElementById("forest");
   HTMLcontext.drawImage(img, 0, 0);
 
   let sceneElement = document.querySelector("a-scene");
 
-  let num_cylinders = 12;
+  let num_cylinders = 16;
   let bubble_shooter_assy = document.createElement("a-entity");
-  bubble_shooter_assy.setAttribute("id", 'bubble-shooter');
+  bubble_shooter_assy.setAttribute("id", "bubble-shooter");
   sceneElement.appendChild(bubble_shooter_assy);
 
   for (let i = 0; i < num_cylinders; i++) {
     let entity = document.createElement("a-entity");
-    entity.setAttribute("position", {x: 8, y: 0, z: 0});
+    entity.setAttribute("position", { x: 8, y: 0, z: 0 });
     entity.setAttribute("id", i);
     let cyl = document.createElement("a-entity");
-    entity.setAttribute("rotation", { x: 0, y: 360 / num_cylinders * i, z: 0 });
+    entity.setAttribute("rotation", {
+      x: 0,
+      y: (360 / num_cylinders) * i,
+      z: 0,
+    });
     cyl.setAttribute("bubble-shooter", "");
     cyl.setAttribute("material", "side: double; opacity: 0.5");
-    cyl.setAttribute("position", {x: 1, y: 0.5, z: 0});
+    cyl.setAttribute("position", { x: 1, y: 0.8, z: 0 });
     let fan = document.createElement("a-entity");
-    fan.setAttribute("fan", '');
+    fan.setAttribute("fan", "");
     fan.setAttribute("id", i);
-    fan.setAttribute("position", {x: 0, y: -0.4, z: 0});
-    fan.setAttribute('raycaster-listen', '');
+    fan.setAttribute("position", { x: 0, y: -0.4, z: 0 });
+    fan.setAttribute("raycaster-listen", "");
     let ball = document.createElement("a-entity");
     ball.setAttribute("ball", "");
-    ball.setAttribute("position", {x: 0, y: 0, z: 0});    
+    ball.setAttribute("position", { x: 0, y: 0, z: 0 });
+    ball.setAttribute("material", "color: #6df4ff; metalness: 0.8");
     cyl.appendChild(ball);
     cyl.appendChild(fan);
     entity.appendChild(cyl);
@@ -126,35 +137,76 @@ $(document).ready(function () {
   }
 
   // var nLasers	= 14
-	// for(var i = 0; i < nLasers; i++){
-	// 	(function(){
+  // for(var i = 0; i < nLasers; i++){
+  // 	(function(){
   //     var laserBeam	= new THREEx.LaserBeam()
   //     let laserBeamEl = document.createElement('a-entity');
   //     sceneElement.appendChild(laserBeamEl)
-      
-	// 		var laserCooked	= new THREEx.LaserCooked(laserBeam)
-	// 		// onRenderFcts.push(function(delta, now){
-	// 		// 	laserCooked.update(delta, now)
-	// 		// })	
-	// 		var object3d		= laserBeam.object3d
-	// 		object3d.position.x	= (i-nLasers/2)/2			
-	// 		object3d.position.y	= 4
-	// 		object3d.rotation.z	= -Math.PI/2			
-	// 	})()
+
+  // 		var laserCooked	= new THREEx.LaserCooked(laserBeam)
+  // 		// onRenderFcts.push(function(delta, now){
+  // 		// 	laserCooked.update(delta, now)
+  // 		// })
+  // 		var object3d		= laserBeam.object3d
+  // 		object3d.position.x	= (i-nLasers/2)/2
+  // 		object3d.position.y	= 4
+  // 		object3d.rotation.z	= -Math.PI/2
+  // 	})()
   // }
-  
+
+  // connect
+  Tone.Transport.bpm.value = bpm;
+
+  const synthOptions = {
+    attackNoise: 1,
+    dampening: 6000,
+    resonance: 0.7,
+    volume: 0,
+  };
+
+  ball_scale = ["C3", "D3", "Eb3", "F3", "G3", "Ab3", "Bb3", "C4"];
+  ball_scale_fract = new Array(num_cylinders);
+  ball_notes = new Array(num_cylinders);
+  for (let i = 0; i < ball_scale_fract.length; i++){
+    ball_scale_fract[i] = 0;
+    ball_notes[i] = ball_scale[ball_scale_fract[i]];
+  }
+  const synth = new Tone.PluckSynth(synthOptions);
+  synth.toMaster();
+
+  seq = new Tone.Sequence(
+    function (time, note) {
+      synth.triggerAttackRelease(note, "4n", time);
+    },
+    ball_notes,
+    "4n"
+  ).start(0);
+  seq.loop = true;
+  // seq.humanize = true;
+
+  Tone.Transport.start();
+
+  function create_note(pt) {
+    let fract = get_fraction_of_canvas(pt);
+    seq.add(
+      get_secs_from_canvas_fract(fract.x),
+      get_note_from_canvas_fract(fract.y)
+    );
+  }
+
   // periodically check for raindrops falling through the floor
   setInterval(() => {
-    let ripple_surface = document.querySelector('#ripple-surface');
+    let ripple_surface = document.querySelector("#ripple-surface");
     // loop through raindrops
-    let raindrops = document.querySelectorAll('[raindrop]');
-    for (let i = 0; i < raindrops.length; i++){
-      if (raindrops[i].object3D.position.y < ripple_surface.object3D.position.y){
+    let raindrops = document.querySelectorAll("[raindrop]");
+    for (let i = 0; i < raindrops.length; i++) {
+      if (
+        raindrops[i].object3D.position.y < ripple_surface.object3D.position.y
+      ) {
         raindrops[i].remove();
       }
     }
   }, 100);
-
 });
 
 AFRAME.registerComponent("canvas-updater", {
@@ -193,11 +245,11 @@ AFRAME.registerComponent("raycaster-listen", {
     // hardcode the raycaster.
     this.el.addEventListener("raycaster-intersected", (evt) => {
       this.raycaster = evt.detail.el;
-      console.log('normal', this.raycaster);
+      // console.log("normal", this.raycaster);
     });
-    // this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
-    //   this.raycaster = null;
-    // });
+    this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
+      this.raycaster = null;
+    });
   },
 
   tick: function () {
@@ -211,25 +263,24 @@ AFRAME.registerComponent("raycaster-listen", {
     if (!intersection) {
       return;
     }
-    console.log(intersection);
-    if (intersection.uv){
+    // console.log(intersection);
+    if (intersection.uv) {
       u = 1.0 - intersection.uv["x"];
       v = intersection.uv["y"];
       // console.log(u, v);
     }
 
     if (spacebar && ready_to_add_hit) {
-
-      //cloud 
-      if (intersection.object.el.id == "cloud"){
+      //cloud
+      if (intersection.object.el.id == "cloud") {
         let pt = intersection.point;
         // console.log(this.el.object3D.worldToLocal(intersection.object.parent.parent.position));
         let raindrop = document.createElement("a-entity");
         raindrop.setAttribute("raindrop", "");
         raindrop.setAttribute("position", pt);
         raindrop.setAttribute("material", "opacity: 0.5");
-        
-        let scene = document.querySelector('a-scene');
+
+        let scene = document.querySelector("a-scene");
         // console.log(scene);
         scene.appendChild(raindrop);
       }
@@ -237,23 +288,25 @@ AFRAME.registerComponent("raycaster-listen", {
       //fan
       if (intersection.object.el.attributes[0].name == "fan") {
         // https://aframe.io/docs/1.0.0/introduction/javascript-events-dom-apis.html#looping-over-entities-from-queryselectorall
-        var fans = document.querySelectorAll('[fan]');
-        var balls = document.querySelectorAll('[ball]');
+        var fans = document.querySelectorAll("[fan]");
+        var balls = document.querySelectorAll("[ball]");
         for (var i = 0; i < fans.length; i++) {
-          if (i == intersection.object.el.id){
+          if (i == intersection.object.el.id) {
             fans[i].components.fan.increase();
             balls[i].components.ball.raise();
+            ball_scale_fract[i] = Math.floor(balls[i].object3D.position.y * ball_scale.length);
+            seq.add(i, ball_scale[ball_scale_fract[i]]);
           }
         }
       }
 
-      if (intersection.object.el.id == "forest-wall"){
+      if (intersection.object.el.id == "forest-wall") {
         let pt = intersection.point;
         // console.log(pt);
         let firefly = document.createElement("a-entity");
         firefly.setAttribute("firefly", "");
-        firefly.setAttribute("position", pt);     
-        let scene = document.querySelector('a-scene');
+        firefly.setAttribute("position", pt);
+        let scene = document.querySelector("a-scene");
         scene.appendChild(firefly);
       }
       ready_to_add_hit = false;
@@ -284,15 +337,14 @@ AFRAME.registerComponent("raycaster-listen-mountain", {
     // Use events to figure out what raycaster is listening so we don't have to
     // hardcode the raycaster.
     this.el.addEventListener("raycaster-intersected", (evt) => {
-      console.log('raycaster-intersected');
+      // console.log("raycaster-intersected");
       this.raycaster = evt.detail.el;
-      console.log('mtn', this.raycaster);
-
+      // console.log("mtn", this.raycaster);
     });
-    // this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
-    //   console.log(evt);
-    //   this.raycaster = null;
-    // });
+    this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
+      // console.log(evt);
+      this.raycaster = null;
+    });
   },
 
   tick: function () {
@@ -306,8 +358,7 @@ AFRAME.registerComponent("raycaster-listen-mountain", {
     if (!intersection) {
       return;
     }
-    console.log(intersection);
-
+    // console.log(intersection);
   },
 });
 // });
@@ -342,31 +393,42 @@ AFRAME.registerComponent("bubble-shooter", {
 
 AFRAME.registerComponent("raindrop", {
   schema: {
-    acc: { type: 'vec3', default: {x: 0, y: -0.000001, z: 0}},
-    vel: { type: 'vec3', default: {x: 0, y: 0, z: 0}}
+    acc: { type: "vec3", default: { x: 0, y: -0.000001, z: 0 } },
+    vel: { type: "vec3", default: { x: 0, y: 0, z: 0 } },
   },
   multiple: true,
 
   init: function () {
     let el = this.el;
     this.geometry = new THREE.SphereGeometry(0.007, 16, 16);
-    this.material = new THREE.MeshBasicMaterial({color: 0x6df4ff});
+    this.material = new THREE.MeshBasicMaterial({ color: 0x6df4ff });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     el.setObject3D("mesh", this.mesh);
   },
 
   tick: function (t, dt) {
-    let pos = this.el.getAttribute('position');
+    let pos = this.el.getAttribute("position");
     this.data.vel.y = this.data.vel.y + dt * this.data.acc.y;
-    this.el.setAttribute('position', {x: pos.x, y: pos.y + this.data.vel.y * dt, z: pos.z});
-    let ripple_surface = document.querySelector('#ripple-surface');
+    this.el.setAttribute("position", {
+      x: pos.x,
+      y: pos.y + this.data.vel.y * dt,
+      z: pos.z,
+    });
+    let ripple_surface = document.querySelector("#ripple-surface");
     // console.log(ripple_surface);
-    if (this.el.getAttribute('position').y < ripple_surface.object3D.position.y) {
-      let cloud_room_pos = document.querySelector('#cloud-room').getAttribute('position');
-      ripple_u = 1.0 - ((cloud_room_pos.x - this.el.getAttribute('position').x) / 2.5 + 0.5);
-      ripple_v = (cloud_room_pos.z - this.el.getAttribute('position').z) / 2.0 + 0.5;      
+    if (
+      this.el.getAttribute("position").y < ripple_surface.object3D.position.y
+    ) {
+      let cloud_room_pos = document
+        .querySelector("#cloud-room")
+        .getAttribute("position");
+      ripple_u =
+        1.0 -
+        ((cloud_room_pos.x - this.el.getAttribute("position").x) / 2.5 + 0.5);
+      ripple_v =
+        (cloud_room_pos.z - this.el.getAttribute("position").z) / 2.0 + 0.5;
     }
-  }
+  },
 });
 
 AFRAME.registerComponent("ball", {
@@ -375,56 +437,65 @@ AFRAME.registerComponent("ball", {
   init: function () {
     let el = this.el;
     this.geometry = new THREE.SphereGeometry(0.15, 16, 16);
-    this.material = new THREE.MeshBasicMaterial({color: 0x6df4ff});
+    this.material = new THREE.MeshBasicMaterial();
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     el.setObject3D("mesh", this.mesh);
   },
 
   tick: function (t, dt) {
-    let rand_incr = 0;// Math.random() * 2.0 - 1.0;
+    let rand_incr = 0; // Math.random() * 2.0 - 1.0;
     let noise_factor = 5e-5;
-    let pos = this.el.getAttribute('position');
-    this.el.setAttribute('position', {x: 0, y: Math.max(pos.y + rand_incr*noise_factor*dt, -0.4), z: 0});
+    let pos = this.el.getAttribute("position");
+    this.el.setAttribute("position", {
+      x: 0,
+      y: Math.max(pos.y + rand_incr * noise_factor * dt, -0.4),
+      z: 0,
+    });
   },
 
-  raise: function() {
+  raise: function () {
     let raise_incr = 0.05;
-    let pos = this.el.getAttribute('position');
-    this.el.setAttribute('position', {x: 0, y: Math.min(pos.y + raise_incr, 1.0) , z: 0}); // TODO slerp
-  }
+    let pos = this.el.getAttribute("position");
+    this.el.setAttribute("position", {
+      x: 0,
+      y: Math.min(pos.y + raise_incr, 1.0),
+      z: 0,
+    }); // TODO slerp
+  },
 });
 
 AFRAME.registerComponent("firefly", {
   init: function () {
     let el = this.el;
     this.geometry = new THREE.SphereGeometry(0.01, 16, 16);
-    this.material = new THREE.MeshBasicMaterial({color: 0xC7C34D});
+    this.material = new THREE.MeshBasicMaterial({ color: 0xc7c34d });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     el.setObject3D("mesh", this.mesh);
   },
 
   tick: function (t, dt) {
-    this.el.object3D.el.components.firefly.material.opacity = (Math.sin(t*0.002)+1.0)/2.0;// .color too
+    this.el.object3D.el.components.firefly.material.opacity =
+      (Math.sin(t * 0.002) + 1.0) / 2.0; // .color too
     // console.log(this.el);
   },
 });
 
 AFRAME.registerComponent("fan", {
   schema: {
-    omega: { type: "vec3", default: {x: 0, y: 0.1, z: 0} },
+    omega: { type: "vec3", default: { x: 0, y: 0.1, z: 0 } },
   },
   multiple: true,
   init: function () {
-    this.el.setAttribute('scale', {x: 0.2, y: 0.2, z: 0.2});
-    this.el.setAttribute('rotation', {x: 0, y: 0, z: 0});
-    this.el.setAttribute('obj-model', 'obj: #fan; mtl: #fan;');
+    this.el.setAttribute("scale", { x: 0.2, y: 0.2, z: 0.2 });
+    this.el.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+    this.el.setAttribute("obj-model", "obj: #fan; mtl: #fan;");
   },
   tick: function (t, dt) {
     let omega = this.data.omega;
-    let rot = this.el.getAttribute('rotation');
-    this.el.setAttribute('rotation', {x: 0, y: rot.y + omega.y*dt , z: 0})
+    let rot = this.el.getAttribute("rotation");
+    this.el.setAttribute("rotation", { x: 0, y: rot.y + omega.y * dt, z: 0 });
   },
-  increase: function() {
+  increase: function () {
     this.data.omega.y += 0.03;
-  }
+  },
 });
